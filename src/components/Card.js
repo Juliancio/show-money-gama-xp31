393994792409712f4f9122f0c23a70db94585c51
Glauber Ariel Magalhães <glaubermagalhaes@outlook.com>
2020-03-17 00:06:17 -0300

    update card.js
    aplicado css para responsividade e estilos conforme mockup

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,22 +3,36 @@ import Fidelize from './assets/fidelize.png'; // Tell webpack this JS file uses 
 import Aumente from './assets/aumente.png'; // Tell webpack this JS file uses this image
 import Adquira from './assets/adquira.png'; // Tell webpack this JS file uses this image
 
+import './style.css';
+
 
 function Card() {   
     return (
 
         <div>
-            <h1>Vantagens Pensando Digital</h1>
-            <h2>Aprenda mais sobre o mercado digital rapidamente!</h2>
+            <h1 className="h1">Vantagens Pensando Digital</h1>
+            <h2 className="h2">Aprenda mais sobre o mercado digital rapidamente!</h2>    
 
-            <img src={Fidelize} alt="Fidelize" />
-            <img src={Aumente} alt="Aumente" />
-            <img src={Adquira} alt="Logo" />
+<div className="row">
+    <div className="container">
+            <div className="col-md-4">            
+              <img src={Fidelize} className="card-img" alt="Fidelize" />
+              <p className="card-p"> Fidelize seus cliente </p>
+            </div>
 
+            <div className="col-md-4">      
+                <img src={Aumente} className="card-img" alt="Aumente" />
+                <p className="card-p"> Aumente sua renda </p>
+            </div>
+
+             <div className="col-md-4">      
+                 <img src={Adquira} className="card-img" alt="Logo" />
+                <p className="card-p">Adquira conhecimento </p>
+            </div>
+        </div>
+</div>
 
         </div>
-
-     
 
     )
 }
