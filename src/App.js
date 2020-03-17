c9d2b0016aeb723bd030d29f161ac9e1bab279e8
Glauber Ariel Magalhães <glaubermagalhaes@outlook.com>
2020-03-16 23:28:55 -0300

    add card.js with img

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Banner from './components/Banner'
+import Card from './components/Card'
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Banner />
+      <Card />
+
     </div>
   );
 }
