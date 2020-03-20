6e84c794fb0bcc7a2f262da6e7a77a145f2f2f0a
Glauber Ariel Magalhães <glaubermagalhaes@outlook.com>
2020-03-20 19:26:48 -0300

    update banner.js
    modificando conteúdo conforme alteração de design pela equipe de ux

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
-import { Player } from 'video-react';
+import Mulher from './assets/mulher-banner.png'; // Tell webpack this JS file uses this image
 
-import './video-react.css';
 
 import './style.css';
 
@@ -26,18 +25,16 @@ function Banner() {
         <div className="hero">
             <div className="description">
                 <h1>Comece agora a <span className="text-transfor">transformação digital</span> da sua empresa</h1>
-                <p>A <span className="brand">Pensando Digital</span> veio para ajudar você a conseguir mais clientes e mais
-                sucesso dentro do mercado digital.
-                Confira o que podemos te oferecer.
+                <p>A Consultoria "Pensando Digital" acelera performance e resultados, trazendo clientes e consequentemente, sucesso dentro do mercado digital para pequenas e médias empresas de todos os segmentos. Confira!
             </p>
                 <div className={classes.root}>
                     <Button variant="contained">CONTRATE AGORA</Button>
                 </div>
             </div>
-            <div className="video">
-                {/* <Player>
-                    <source src="https://automaho.000webhostapp.com/cardb.mp4" />
-                </Player> */}
+            <div className="mulher-sorrindo">
+                <img src={Mulher} className="mulher-img" alt="Logo" />
+
+
             </div>
         </div>
     )
