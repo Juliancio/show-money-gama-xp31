5970daeb9dba11cd3f7a411cda2c6d3394ae905c
Glauber Ariel Magalhães <glaubermagalhaes@outlook.com>
2020-03-23 02:11:16 -0300

    update logo img

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Logo from './assets/logo.png';
+import Logo from './assets/logo-pensando.svg';
 import Button from '@material-ui/core/Button';
 import './style.css';
 
@@ -13,7 +13,6 @@ function Nav() {
             <div className="logo">
                 <img src={Logo} alt="Fidelize" />
             </div>
-
             <div className='btn-contrate'>
                 <Button onClick={() => window.open('https://www.sympla.com.br/consultoria-pensando-digital__826707', '_blank')} variant="contained">CONTRATE AGORA</Button>
             </div>
